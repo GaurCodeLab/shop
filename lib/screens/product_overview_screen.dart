dcecb4e41d09c6ad44960799624cb03ef59d598a
gaurcodelab <ankit.gaur1023@gmail.com>
2020-01-28 19:37:57 +0530

    initial commit

---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Productoverviewscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
