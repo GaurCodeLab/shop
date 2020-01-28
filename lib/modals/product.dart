12c81c3b7cde4765e426dba626331a4345b5316a
gaurcodelab <ankit.gaur1023@gmail.com>
2020-01-28 19:57:37 +0530

    demo data

--- lib/modals/product.dart
+++ lib/modals/product.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/widgets.dart';
 
-class Product{
-
+class Product {
   final String id;
   final String title;
   final String description;
   final double price;
+  final String imageUrl;
   bool isFavourite = false;
-  
-  Product({@required this.id, @required this.title, @required this.description, @required this. price,this.isFavourite});
-}
 
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.price,
+      @required this.imageUrl,
+      this.isFavourite});
+}
